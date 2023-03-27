02f0d8b6f67557eac27da02f7e94868a8615fa11
Ryan Marin <ryan.marin8629@gmail.com>
2023-03-27 23:47:46 +0200

    favicon

--- src/layout/drawer/NavButton.jsx
+++ src/layout/drawer/NavButton.jsx
@@ -11,8 +11,11 @@ const NavButton = (props) => {
         height: "80px",
         display: "flex",
         alignItems: "center",
-        fontSize: "25px"
+        fontSize: "25px",
+        color: "white",
+        textDecoration: "none",
       }}
+      href="#"
     >
       <div className="nav-button-icon" style={{ width: "30px", height:"100%", padding:"0px 10px" }}/>
       {children}
