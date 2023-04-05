149b1436a724e880a4c035dbb507cb1f82feec37
Ryan Marin <ryan.marin8629@gmail.com>
2023-04-06 00:18:01 +0200

    writer n'existe pas encore

--- src/pages/frezo-writer/FrezoWriter.jsx
+++ src/pages/frezo-writer/FrezoWriter.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const FrezoWriter = () => {
   const location = useLocation();
@@ -9,8 +9,13 @@ const FrezoWriter = () => {
   }, [location]);
 
   return (
-    <h1>Frezo Writer<div>Écrivez des textes en frézo !</div></h1>
-  )
-}
+    <>
+      <h1>
+        Frezo Writer<div>Écrivez des textes en frézo !</div>
+      </h1>
+      N'existe pas encore.
+    </>
+  );
+};
 
-export default FrezoWriter
+export default FrezoWriter;
