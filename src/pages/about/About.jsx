d6f36598806875119956aefb1ceaebb968bd0841
Ryan Marin <ryan.marin8629@gmail.com>
2023-04-05 23:54:19 +0200

    home

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -13,8 +13,8 @@ const About = () => {
   return (
     <Stack spacing={4}>
       <div>
-        Ce site web a été créé par <a href="https://github.com/Ryan-MARIN" style={{textDecoration:"none"}}><b>Ryan Marin</b></a>, <a href="https://github.com/Arseid" style={{textDecoration:"none"}}><b>Yves-Robert Estrada</b></a> et <b>Sinclair
-        Balivet</b>.
+        Ce site web a été créé par <a href="https://github.com/Ryan-MARIN" style={{textDecoration:"none"}}><b>Ryan Marin</b></a>, <a href="https://github.com/Arseid" style={{textDecoration:"none"}}><b>Yves-Robert Estrada</b></a> et <a href="https://github.com/Ryan-MARIN" style={{textDecoration:"none"}}><b>Ryan Marin</b></a>, <a href="https://github.com/Arseid" style={{textDecoration:"none"}}><b>Sinclair
+        Balivet</b></a>.
       </div>
       <div>Avec la participation de <b>Llona André--Augustine</b>.</div>
       <div>Mention spéciale à <b>Samuel Demont</b> et <b>Llona André--Augustine</b> qui m'a aidé à concevoir le Frézo.</div>
