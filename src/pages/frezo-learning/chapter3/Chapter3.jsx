32c29eceb953c023a6f1012de628064b07bd825e
Ryan Marin <ryan.marin8629@gmail.com>
2023-04-06 04:42:48 +0200

    modifs

--- src/pages/frezo-learning/chapter3/Chapter3.jsx
+++ src/pages/frezo-learning/chapter3/Chapter3.jsx
@@ -4,6 +4,21 @@ const Chapter3 = () => {
   return (
     <div>
       <h1>Chapitre 3 : Le système simplifié</h1>
+      <p>
+        Le Frézo simplifié possède une allure plus naturelle que le
+        traditionnelle. La séparation des mots, phrases et paragraphes se fait
+        avec des espaces, des points et des retours à la ligne, comme dans la
+        plupart des langues occidentales.
+      </p>
+      <p>
+        Ainsi, les lignes de lecture disparaissent et les décorateurs n'existent
+        plus. Il est cependant toujours possible d'utiliser les idéogrammes et
+        les caractères numériques. À ce propos, le système de numération est
+        différent du Frézo traditionnel. En effet, les nombres s'écrivent comme
+        en français, directement avec des chiffres les uns à la suite des
+        autres. Les caractères de dizaine, de centaine et de millier ne sont pas
+        utilisables.
+      </p>
     </div>
   );
 };
--- src/pages/frezo-learning/chapter4/Exercice1.jsx
+++ src/pages/frezo-learning/chapter4/Exercice1.jsx
@@ -1,54 +1,69 @@
-import React from 'react';
-import AlphabetFrezo from '../../../ressources/alphabet-frezo.json';
+import React from "react";
+import AlphabetFrezo from "../../../ressources/alphabet-frezo.json";
+import { Stack } from "@mui/system";
+import { Grid } from "@mui/material";
 
 const Exercice1 = () => {
-    const [currentSymbol, setCurrentSymbol] = React.useState(null);
+  const [currentSymbol, setCurrentSymbol] = React.useState(null);
 
-    const generateRandomSymbol = () => {
-        const randomIndex = Math.floor(Math.random() * AlphabetFrezo['chiffres'].length);
-        const randomSymbol = AlphabetFrezo['chiffres'][randomIndex];
-        setCurrentSymbol(randomSymbol);
-    };
-
-    React.useEffect(() => {
-        generateRandomSymbol();
-    }, []);
-
-    const handleLetterClick = (number) => {
-        if (number === currentSymbol) {
-            alert("Correct!");
-            generateRandomSymbol();
-        } else {
-            alert("Incorrect. Try again!");
-        }
-    };
-
-    const renderNumbers = () => {
-        const buttons = [];
-
-        Object.values(AlphabetFrezo['chiffres']).forEach((number, index) => {
-            buttons.push(
-                <button key={index} onClick={() => handleLetterClick(number)}>
-                    {number}
-                </button>
-            );
-        });
-
-        return buttons;
-    };
-
-    return (
-        <div className="App">
-            <h1>Exercice 1</h1>
-            {currentSymbol && (
-                <p>
-                    Frezo: <span className='frezo'>{currentSymbol}</span><br/>
-                    Son: <br/>
-                    {renderNumbers()}
-                </p>
-            )}
-        </div>
+  const generateRandomSymbol = () => {
+    const randomIndex = Math.floor(
+      Math.random() * AlphabetFrezo["chiffres"].length
     );
-}
+    const randomSymbol = AlphabetFrezo["chiffres"][randomIndex];
+    setCurrentSymbol(randomSymbol);
+  };
+
+  React.useEffect(() => {
+    generateRandomSymbol();
+  }, []);
+
+  const handleLetterClick = (number) => {
+    if (number === currentSymbol) {
+      alert("Correct!");
+      generateRandomSymbol();
+    } else {
+      alert("Incorrect. Try again!");
+    }
+  };
+
+  const renderNumbers = () => {
+    const buttons = [];
+
+    Object.values(AlphabetFrezo["chiffres"]).forEach((number) => {
+      buttons.push(
+        <Grid item key={number} xs={2} md={1}>
+          <button
+            style={{ width: "100%", height: "100%" }}
+            onClick={() => handleLetterClick(number)}
+          >
+            {number}
+          </button>
+        </Grid>
+      );
+    });
+
+    return buttons;
+  };
+
+  return (
+    <div>
+      <h1>Exercice 1</h1>
+      {currentSymbol && (
+        <Stack alignItems={"center"}>
+          <span className="frezo" style={{ fontSize: "100px" }}>
+            {currentSymbol}
+          </span>
+          Cliquez sur le son correspondant à cette lettre :
+          <p>
+            <Grid columns={10} container spacing={1} justifyContent={"center"}>
+              {renderNumbers()}
+            </Grid>
+          </p>
+        </Stack>
+      )}
+    </div>
+  );
+};
 
 export default Exercice1;
